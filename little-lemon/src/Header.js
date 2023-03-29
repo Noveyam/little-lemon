7b57493ee15b4609f0cf4a43dd578c03037ad499
noveycea@gmail.com <noveycea@gmail.com>
2023-03-29 15:42:16 -0700

    removed test.txt & add more meta tags to header

--- little-lemon/src/Header.js
+++ little-lemon/src/Header.js
@@ -4,6 +4,15 @@ function Header() {
     return (
         <head>
             <title>Litte Lemon</title>
+            <meta name="author" content="Novey"/>
+            <meta name="title" content="Little Lemon"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="description" content="Come visit Little Lemon Restaurant"/>
+            <meta name="keywords" content="Little Lemon, Restaurant"/>
+            <meta name="format-detection" content="telephone=yes"/> 
+            <meta name="HandheldFriendly" content="true"/>
+            <meta name="revised" content="Wednesday, March 29th, 2023, 03:38 PM"/>
+            
             <meta property="og:title" content="Little Lemon Restaurant" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="http://littlelemon.com" />
